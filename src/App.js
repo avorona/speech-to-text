e03a5592ac1cabfe088a1afd4aeac53d7fd9a60c
avorona <vitalii.parshikov@gmail.com>
2018-07-02 00:56:10 +0300

    Initialize App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="text">
+          It's an app to transform recorded audio and speech to the text file. I
+          had come up with this idea when I thought about that it would be great
+          to transform my audio records into the formal text. I know that there
+          are a lot of solutions from Google and other which made this process
+          easy. But it cost a little bit. And I decided to train my own neural
+          network using JavaScript. Wish me luck.
+        </p>
+        <div className="controll">
+          <button className="button">Click me</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
